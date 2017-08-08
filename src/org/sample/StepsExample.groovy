2caf3a1db75f6fa57e93c7b0ed19a5baef6b5d6e
Ankit-Arni <ankit.arni.1995@gmail.com>
2017-08-08 19:28:44 +0530

    Create StepsExample.groovy

---
+++ src/org/sample/StepsExample.groovy
@@ -0,0 +1,8 @@
+
+class Utilities implements Serializable {
+  def steps
+  Utilities(steps) {this.steps = steps}
+  def mvn(args) {
+    steps.batchFile "${steps.tool 'Maven'}/bin/mvn -o ${args}"
+  }
+}
