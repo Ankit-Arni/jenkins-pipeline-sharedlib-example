52d498d6db3ecba14bc382015dd0eafef1cf223f
Ankit-Arni <ankit.arni.1995@gmail.com>
2017-08-08 19:28:15 +0530

    Delete StepsExample.groovy

--- src/StepsExample.groovy
+++
@@ -1,7 +0,0 @@
-class Utilities implements Serializable {
-  def steps
-  Utilities(steps) {this.steps = steps}
-  def mvn(args) {
-    steps.batchFile "${steps.tool 'Maven'}/bin/mvn -o ${args}"
-  }
-}
